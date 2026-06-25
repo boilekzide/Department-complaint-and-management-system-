2d83cda6d3e8e8df21e4d25859f9f6f3ee7972e8
boilekzide <olalekanadebodun@gmail.com>
2026-06-25 16:58:20 +0100

    Added database connection and schema

--- database/create_admin.php
+++ database/create_admin.php
@@ -1,1 +1,17 @@
+<?php
+include "../config/db_connect.php";
 
+$fullname = "Olalekan Adebodun";
+$username = "admin1";
+$email = "olalekanadebodun@gmail.com";
+$password = password_hash("123456", PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO admins(fullname, username, email, password)
+        VALUES('$fullname', '$username', '$email', '$password')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Admin created successfully";
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+?>
