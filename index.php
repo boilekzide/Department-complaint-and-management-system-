582fcef0b25d23e592313e0f5f47eeac2ce15bea
boilekzide <olalekanadebodun@gmail.com>
2026-06-29 18:43:48 +0100

    Ui team working on the project, added a new logo and made some style adjustments in the CSS file. Updated the index.php to reflect the changes in the UI.

--- index.php
+++ index.php
@@ -1,1 +1,37 @@
+<?php include "includes/header.php"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        Departmental Complaint System
+    </title>
+</head>
+<body>
+    <div class="logo-box">
+        <img src="logo.png" alt="School Logo">
+        <h1>Official Student Complaint system</h1>
+    </div>
+<h1>
+Departmental Complaint Management System
+</h1>
 
+<hr>
+
+<a href="student/register.php">
+Student Registration
+</a>
+
+<br><br>
+
+<a href="student/login.php">
+Student Login
+</a>
+
+<br><br>
+
+<a href="admin/login.php">
+Admin Login
+</a>
+</body>
+</html>
+<?php include "includes/footer.php"; ?>  
